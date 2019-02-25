6fa5b1d5e98193e02587a8313dbc8c90915a9d5d
Alexkcshing <alexkcshing@gmail.com>
2019-02-25 14:37:14 +0000

    setup file structure

---
+++ src/models/prime_checker.js
@@ -0,0 +1,11 @@
+PrimeChecker.prototype.numberIsPrime = function (number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        return false;
+    }
+  }
+  return true;
+};
