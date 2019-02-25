ea78892a2a062b981f887f94b1703d0e7871aa78
Alexkcshing <alexkcshing@gmail.com>
2019-02-25 14:48:54 +0000

    set up form submit event listener

--- src/views/form_view.js
+++ src/views/form_view.js
@@ -3,7 +3,14 @@ const FormView = function (){
 };
 
 FormView.prototype.bindEvents = function () {
+  const form = document.querySelector("#prime-checker-form");
+  form.addEventListener("submit", (event) => {
 
+    event.preventDefault();
+
+    const numberInput = event.target.number.value;
+    PubSub.publish("FormView:number-input", numberInput);
+  })
 };
 
 module.exports = FormView;
